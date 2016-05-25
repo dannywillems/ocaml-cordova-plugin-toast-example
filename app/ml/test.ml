cd26bec9400fc80756280836dc5a84136b84c41e
Danny Willems <contact@danny-willems.be>
2016-05-25 18:39:35 +0200

    Better design

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -10,56 +10,70 @@ type action =
 let action_to_str a = match a with
   | ShortTop -> "Short Top"
   | ShortBottom -> "Short Bottom"
-  | ShortCenter -> "Short Center"
+  | ShortCenter -> "Short center"
   | LongTop -> "Long Top"
   | LongBottom -> "Long Bottom"
   | LongCenter -> "Long Center"
   | Hide -> "Hide"
 
-let new_button action text =
+let new_button action =
+  let toast = Cordova_toast.t () in
+
   let row = Dom_html.createDiv Dom_html.document in
   let col = Dom_html.createDiv Dom_html.document in
   let button = Dom_html.createButton Dom_html.document in
+  let t = action_to_str action in
   row##.className := (Js.string "row");
   col##.className := (Js.string "col s12");
   button##.className := (Js.string "waves-effect waves-light btn");
-  button##.innerHTML := (Js.string (action_to_str action));
-  Lwt.async
-  ( fun () ->
-    Lwt_js_events.clicks button
-    ( fun _ev _thread ->
-      let t = Toast.toast () in
-      ignore (match action with
-      | ShortTop -> t##(showShortTop (Js.string text))
-      | ShortBottom -> t##showShortBottom (Js.string text)
-      | ShortCenter -> t##showShortCenter (Js.string text)
-      | LongTop -> t##showLongTop (Js.string text)
-      | LongBottom -> t##showLongBottom (Js.string text)
-      | LongCenter -> t##showLongCenter (Js.string text)
-      | Hide -> t##hide);
-      Lwt.return ()
-    )
+  button##.innerHTML := (Js.string t);
+
+  (*
+  Lwt_js_events.clicks button
+  ( fun ev thread ->
+    ignore (match action with
+    | ShortTop -> toast#show_short_top text
+    | ShortBottom -> toast#show_short_bottom text
+    | ShortCenter -> toast#show_short_center text
+    | LongTop -> toast#show_long_top text
+    | LongBottom -> toast#show_long_bottom text
+    | LongCenter -> toast#show_long_center text
+    | Hide -> toast#hide);
+    Lwt.return ()
   );
+  *)
+
+  button##.onclick := Dom.handler
+  (
+    fun e ->
+    ignore (match action with
+    | ShortTop -> toast#show_short_top t
+    | ShortBottom -> toast#show_short_bottom t
+    | ShortCenter -> toast#show_short_center t
+    | LongTop -> toast#show_long_top t
+    | LongBottom -> toast#show_long_bottom t
+    | LongCenter -> toast#show_long_center t
+    | Hide -> toast#hide);
+    Js._false
+  );
+
   Dom.appendChild col button;
   Dom.appendChild row col;
   row
 
 let on_device_ready _ =
-  (*
-  let opt = Toast.create_options "Hello world" Toast.short
-  Toast.top (Toast.create_toast_styling ()) in
-  t##showWithOptions opt;
-  *)
   let doc = Dom_html.document in
   let div = Dom_html.createDiv doc in
   div##.className := (Js.string "container center");
-  Dom.appendChild div (new_button ShortBottom "Hello world");
-  Dom.appendChild div (new_button ShortCenter "Hello world");
-  Dom.appendChild div (new_button ShortTop "Hello world");
-  Dom.appendChild div (new_button LongBottom "Hello world");
-  Dom.appendChild div (new_button LongCenter "Hello world");
-  Dom.appendChild div (new_button LongTop "Hello world");
-  Dom.appendChild div (new_button Hide "");
+
+  Dom.appendChild div (new_button ShortTop);
+  Dom.appendChild div (new_button ShortBottom);
+  Dom.appendChild div (new_button ShortCenter);
+  Dom.appendChild div (new_button LongTop);
+  Dom.appendChild div (new_button LongBottom);
+  Dom.appendChild div (new_button LongCenter);
+  Dom.appendChild div (new_button Hide);
+
   Dom.appendChild doc##.body div;
   Js._false
 
