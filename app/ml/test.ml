0e48e642fe8bb87107f39e0b30362e98809e5f37
Danny Willems <contact@danny-willems.be>
2016-06-17 22:32:12 +0200

    Use functions instead of objects

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -6,6 +6,7 @@ type action =
   | LongBottom
   | LongCenter
   | Hide
+  | Styling
 
 let action_to_str a = match a with
   | ShortTop -> "Short Top"
@@ -15,10 +16,9 @@ let action_to_str a = match a with
   | LongBottom -> "Long Bottom"
   | LongCenter -> "Long Center"
   | Hide -> "Hide"
+  | Styling -> "Styling"
 
 let new_button action =
-  let toast = Cordova_toast.t () in
-
   let row = Dom_html.createDiv Dom_html.document in
   let col = Dom_html.createDiv Dom_html.document in
   let button = Dom_html.createButton Dom_html.document in
@@ -32,13 +32,13 @@ let new_button action =
   Lwt_js_events.clicks button
   ( fun ev thread ->
     ignore (match action with
-    | ShortTop -> toast#show_short_top text
-    | ShortBottom -> toast#show_short_bottom text
-    | ShortCenter -> toast#show_short_center text
-    | LongTop -> toast#show_long_top text
-    | LongBottom -> toast#show_long_bottom text
-    | LongCenter -> toast#show_long_center text
-    | Hide -> toast#hide);
+    | ShortTop -> Cordova_toast.show_short_top text
+    | ShortBottom -> Cordova_toast.show_short_bottom text
+    | ShortCenter -> Cordova_toast.show_short_center text
+    | LongTop -> Cordova_toast.show_long_top text
+    | LongBottom -> Cordova_toast.show_long_bottom text
+    | LongCenter -> Cordova_toast.show_long_center text
+    | Hide -> Cordova_toast.hide);
     Lwt.return ()
   );
   *)
@@ -47,13 +47,34 @@ let new_button action =
   (
     fun e ->
     ignore (match action with
-    | ShortTop -> toast#show_short_top t
-    | ShortBottom -> toast#show_short_bottom t
-    | ShortCenter -> toast#show_short_center t
-    | LongTop -> toast#show_long_top t
-    | LongBottom -> toast#show_long_bottom t
-    | LongCenter -> toast#show_long_center t
-    | Hide -> toast#hide);
+    | ShortTop -> Cordova_toast.show_short_top t ()
+    | ShortBottom -> Cordova_toast.show_short_bottom t ()
+    | ShortCenter -> Cordova_toast.show_short_center t ()
+    | LongTop -> Cordova_toast.show_long_top t ()
+    | LongBottom -> Cordova_toast.show_long_bottom t ()
+    | LongCenter -> Cordova_toast.show_long_center t ()
+    | Styling ->
+        Cordova_toast.show_with_options
+          ~options:
+          (
+            Cordova_toast.create_options
+              ~message:"Styling"
+              ~duration:Cordova_toast.Long
+              ~position:Cordova_toast.Center
+              ~styling:
+              (
+                Cordova_toast.create_styling_toast
+                  ~opacity:0.9
+                  ~background_color:"red"
+                  ~corner_radius:20
+                  ~vertical_padding:30
+                  ~horizontal_padding:30
+                  ()
+              )
+              ()
+          )
+          ()
+    | Hide -> Cordova_toast.hide ());
     Js._false
   );
 
@@ -73,6 +94,7 @@ let on_device_ready _ =
   Dom.appendChild div (new_button LongBottom);
   Dom.appendChild div (new_button LongCenter);
   Dom.appendChild div (new_button Hide);
+  Dom.appendChild div (new_button Styling);
 
   Dom.appendChild doc##.body div;
   Js._false
